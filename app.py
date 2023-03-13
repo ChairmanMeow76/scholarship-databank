15dca77189f2385f4838f51445a5046bed0ae6a5
Quan Chu <quanchu.email@gmail.com>
2023-03-13 07:55:40 +0700

    commit

--- app.py
+++ app.py
@@ -94,7 +94,7 @@ if search_button:
     #fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
     
     #st.plotly_chart(fig,use_container_width=True)
-    st.write(preferred_df[['sponsor','study_level','value','host','link','note','deadline','discipline']])
+    st.write(preferred_df[['sponsor','discipline','study_level','value','host','link','note','deadline']])
     
 
 
